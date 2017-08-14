26ac113e872e0e6677513c81e12b3bcebd0a0223
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-08-13 20:30:21 -0700

    Put databases update in correct spot

--- gpy_site/settings.py
+++ gpy_site/settings.py
@@ -18,7 +18,6 @@ from social_core.pipeline.user import get_username
 import dj_database_url
 
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,6 +92,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators

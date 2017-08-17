4d9345ac75be33205a4ce7bacd7a53b67862828e
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-08-16 19:06:25 -0700

    Remove CSRF for now

--- gpy_site/settings.py
+++ gpy_site/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

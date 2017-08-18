98dc92a5dd904854d8f4c28833bf83ec481b8a5e
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-08-18 13:09:25 -0700

    All users styling

--- gpy_ulx.lua
+++ gpy_ulx.lua
@@ -3,6 +3,7 @@ local CATEGORY_NAME = "G-Py ULX"
 
 function ulx.gpy_sync_data( calling_ply, url, key )
 	ulx_players = {}
+        my_key = "3f440c72-cfcc-480c-981a-d1e474ad63e6"
 	my_url = "https://gpy-gpy.herokuapp.com/key"
     my_fetched_headers = {}
     -- Http fetch to grab headers ---------------------
@@ -25,8 +26,7 @@ function ulx.gpy_sync_data( calling_ply, url, key )
 		end
 	end
 	local json_ulx_players = util.TableToJSON(ulx_players)
-	key = "3f440c72-cfcc-480c-981a-d1e474ad63e6"
-	params = { ulx_secret_key = key,
+        params = { ulx_secret_key = my_key,
 			   ulx_ranks = json_ulx_players, }
 	http.Post( my_url,
         params,
@@ -38,7 +38,7 @@ function ulx.gpy_sync_data( calling_ply, url, key )
 	        print( failed )
 	    end,
         my_fetched_headers )
-        ulx.fancyLogAdmin( calling_ply, true, "#A ran gpy_sync_data command: #s", command )
+        ulx.fancyLogAdmin( calling_ply, true, "#A ran gpy_sync_data command." )
 end
 local gpy_sync_data = ulx.command( CATEGORY_NAME, "ulx gpy_sync_data", ulx.gpy_sync_data, "!gpy_sync_data", true, false, true )
 gpy_sync_data:defaultAccess( ULib.ACCESS_SUPERADMIN )

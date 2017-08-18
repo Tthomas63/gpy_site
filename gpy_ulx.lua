48e3022f99736392020b85bb3de8bca9c45c38be
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-08-18 13:12:27 -0700

    Ulx module edit

--- gpy_ulx.lua
+++ gpy_ulx.lua
@@ -5,21 +5,21 @@ function ulx.gpy_sync_data( calling_ply, url, key )
 	ulx_players = {}
         my_key = "3f440c72-cfcc-480c-981a-d1e474ad63e6"
 	my_url = "https://gpy-gpy.herokuapp.com/key"
-    my_fetched_headers = {}
-    -- Http fetch to grab headers ---------------------
-    http.Fetch(
-        my_url,
-        function( body, len, headers, code )
-            -- The first argument is the HTML we asked for.
-            TheReturnedHTML = body
-            my_fetched_headers = headers
-            print("Response code is: "..code)
-        end,
-        function( error )
-            -- We failed. =(
-        end
-    )
-    -- Http Post to send data --------------------------
+        my_fetched_headers = {}
+        -- Http fetch to grab headers ---------------------
+        http.Fetch(
+                my_url,
+                function( body, len, headers, code )
+                        -- The first argument is the HTML we asked for.
+                        TheReturnedHTML = body
+                        my_fetched_headers = headers
+                        print("Response code is: "..code)
+                end,
+                function( error )
+                        -- We failed. =(
+                end
+        )
+        -- Http Post to send data --------------------------
 	for steamid, table in pairs(ULib.ucl.users) do
 		for attr, value in pairs(table) do
 			ulx_players[steamid] = table['group']
@@ -27,17 +27,17 @@ function ulx.gpy_sync_data( calling_ply, url, key )
 	end
 	local json_ulx_players = util.TableToJSON(ulx_players)
         params = { ulx_secret_key = my_key,
-			   ulx_ranks = json_ulx_players, }
+                ulx_ranks = json_ulx_players, }
 	http.Post( my_url,
-        params,
-        function( result )
-	        if result then
-                print( "Done!" ) end
-	        end,
-        function( failed )
-	        print( failed )
-	    end,
-        my_fetched_headers )
+                params,
+                function( result )
+                        if result then
+                                print( "Done!" ) end
+                end,
+                function( failed )
+                        print( failed )
+                end,
+                my_fetched_headers )
         ulx.fancyLogAdmin( calling_ply, true, "#A ran gpy_sync_data command." )
 end
 local gpy_sync_data = ulx.command( CATEGORY_NAME, "ulx gpy_sync_data", ulx.gpy_sync_data, "!gpy_sync_data", true, false, true )

7b33dc2484be4ea45e0ed69b38a2bd905ada1bd3
Tthomas63 <mr.tyler.thomas@gmail.com>
2017-08-19 20:49:31 -0700

    Init forums

--- apps/forums/models.py
+++ apps/forums/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+
+
+class Thread(models.Model):
+    category = models.ForeignKey('forums.Category', related_name="threads")
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    started_by = models.ForeignKey('main.SteamUser')
+    title = models.CharField(max_length=255)
+    include_signatures = models.BooleanField(default=False)
+    last_post_date = models.DateTimeField(null=True, blank=True)
+
+
+class Post(models.Model):
+    thread = models.ForeignKey('forums.Thread', related_name="posts")
+    date_created = models.DateTimeField()
+    posted_by = models.ForeignKey('main.SteamUser')
+    content = models.TextField(max_length=600)
--- gpy/urls.py
+++ gpy/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.main.urls', namespace="main")),
     url(r'^remote_admin/', include('apps.remote_admin.urls', namespace="remote_admin")),
+    url(r'^forums/', include('apps.forums.urls', namespace="forums")),
     url('', include('social_django.urls', namespace='social')),
 ]
